ea41fcb2c120258130ea13a61ea58209f309f2ed
DeLaPF <de.alafia@gmail.com>
2023-01-13 12:06:52 +0000

    Add git support

--- packer_sync.sh
+++ packer_sync.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+# Get name of user owning packer.lua
+echo "Please enter the name of the user in 'nvim/lua/<user>/packer.lua'"
+read user
+echo "Looking for 'nvim/$user/packer.lua'"
+
 # Weird Packer Shenanigans
-if [ -f "/home/$USER/.config/nvim/lua/$USER/packer.lua" ]; then
-    echo "Running PackerSync"
-    nvim /home/$USER/.config/nvim/lua/$USER/packer.lua --headless \
+if [ -f "$HOME/.config/nvim/lua/$user/packer.lua" ]; then
+    echo "Running 'PackerSync' on 'nvim/$user/packer.lua'"
+    echo "Please close with Ctrl-c after about 5-10s"
+    nvim $HOME/.config/nvim/lua/$user/packer.lua --headless \
         -c 'so' -c 'PackerSync' 2>/dev/null \
-	#& 
-    #pid=$!
-    #sleep 30
-    #kill $pid
+else
+    echo "Error: 'nvim/$user/packer.lua' not found"
 fi
+
+exit
